4aeb2bcad3ce97134762b37819fa621d1e232fec
Artem Khan <akhan195@my.bcit.ca>
2022-09-05 19:49:12 -0700

    Fixed the checkstyle errors.

--- src/comp2522/java/org/example/Main.java
+++ src/comp2522/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+/**
+ * Class has a main method that prints what my favourite  is.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("My favourite animal is a Panda!");
-    }
+  public static void main(String[] args) {
+    System.out.println("My favourite animal is a Panda!");
+  }
 }
