d4a7b8780b552b8c8fdc989fb6a4c6b7d717c4a5
Artem Khan <akhan195@my.bcit.ca>
2022-09-13 08:10:09 -0700

    Edited comments and fixed Checkstyle.

--- src/main/java/org/comp2522/Main.java
+++ src/main/java/org/comp2522/Main.java
@@ -2,8 +2,17 @@ package org.comp2522;
 
 /**
  * Class has a main method that prints what my favourite animal is.
+ *
+ * @author artem
+ * @version 1 / date: 2022-09-08
  */
+
 public class Main {
+
+  /**
+   * Main method that prints the message.
+   *
+   */
   public static void main(String[] args) {
     System.out.println("My favourite animal is a Panda!");
   }
